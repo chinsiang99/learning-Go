bc8a25681916269058c248e0cf75ce0bc8c7fdeb
Tan Chin Siang <chinsiang99@gmail.com>
2024-10-02 15:31:43 +0800

    feat: update some comment to the program

--- app.go
+++ app.go
@@ -1,4 +1,4 @@
-package main
+package main // this is important to be named "main" package, because "main" is a special package name as it tells Go this is the main entry point of the program
 
 import "fmt" // this is a package
 
@@ -6,4 +6,4 @@ func main() {
 	fmt.Print("Hello World")
 }
 
-// run: go run app.go 
+// run: go run app.go
