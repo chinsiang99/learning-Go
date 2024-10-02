647d1209a4a5ad1dc472242b1b7ab80ef2b0f50b
Tan Chin Siang <chinsiang99@gmail.com>
2024-10-03 07:15:51 +0800

    feat: update small project program

--- 01.small-project/investment-calculation.go
+++ 01.small-project/investment-calculation.go
@@ -8,13 +8,22 @@ import (
 func main() {
 	fmt.Println("hello there~ the program started hehe")
 	const inflationRate float64 = 2.5
-	var investmentAmount float64 = 1000
+	// var investmentAmount float64 = 1000
+	var investmentAmount float64
 	// var expectedReturnRate float64 = 5.5
-	expectedReturnRate := 5.5 // infer type recommendation shorter way assigning a variable
+	expectedReturnRate := 3.0 // infer type recommendation shorter way assigning a variable
 	var years float64 = 10
 
 	// var investmentAmount, years float64 = 1000, 10
 	// investmentAmount, years := 1000.0, 10.0
+	fmt.Print("Please input your investment amount: ")
+	fmt.Scan(&investmentAmount) // & meaning pointers
+
+	fmt.Print("Please input your expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Please input your year(s) of investment: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years) // can convert from int to float64 (use float64(years))
 
