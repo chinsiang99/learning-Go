1a0156499b5ffd963721b996de450cd4518b4caa
Tan Chin Siang <chinsiang99@gmail.com>
2025-03-20 11:11:00 +0800

    feat: create a basic calculator for tax and price calculation

--- 13.project-knowledge/app.go
+++ 13.project-knowledge/app.go
@@ -4,4 +4,19 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	// var price []float64 = []float64{10, 20, 30}
+	// objectives - is to calculate the tax for each price
+	price := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.7, 0.1, 0.15}
+
+	priceForEachTaxRate := make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+		priceAfterTax := make([]float64, len(price))
+		for index, price := range price {
+			priceAfterTax[index] = price * (1 + taxRate)
+		}
+		priceForEachTaxRate[taxRate] = priceAfterTax
+	}
+	fmt.Println(priceForEachTaxRate, "this is price for each tax rate")
 }
