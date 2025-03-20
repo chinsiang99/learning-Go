61cc8da521c05c2f21cbb3994c807c221234a49c
Tan Chin Siang <chinsiang99@gmail.com>
2025-03-20 11:39:12 +0800

    feat: adding method to process the values with tax rates

--- 13.project-knowledge/app.go
+++ 13.project-knowledge/app.go
@@ -1,24 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"chinsiang.com/price-calculator/prices"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
 	// var price []float64 = []float64{10, 20, 30}
 	// objectives - is to calculate the tax for each price
-	price := []float64{10, 20, 30}
+	// price := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.7, 0.1, 0.15}
 
-	priceForEachTaxRate := make(map[float64][]float64)
+	// priceForEachTaxRate := make(map[float64][]float64)
 
+	// for _, taxRate := range taxRates {
+	// 	priceAfterTax := make([]float64, len(price))
+	// 	for index, price := range price {
+	// 		priceAfterTax[index] = price * (1 + taxRate)
+	// 	}
+	// 	priceForEachTaxRate[taxRate] = priceAfterTax
+	// }
 	for _, taxRate := range taxRates {
-		priceAfterTax := make([]float64, len(price))
-		for index, price := range price {
-			priceAfterTax[index] = price * (1 + taxRate)
-		}
-		priceForEachTaxRate[taxRate] = priceAfterTax
+		job := prices.NewTaxIncludedPriceJob(taxRate)
+		job.Process()
+		fmt.Println(job.TaxIncludedPrices, "this is price for each tax rate")
 	}
-	fmt.Println(priceForEachTaxRate, "this is price for each tax rate")
+	// fmt.Println(priceForEachTaxRate, "this is price for each tax rate")
 	// for key, value := range priceForEachTaxRate {
 	// 	for index, priceAfterTax := range value {
 	// 		fmt.Println("Tax rate: ", key, "Before tax: ", price[index], "After tax: ", priceAfterTax)
