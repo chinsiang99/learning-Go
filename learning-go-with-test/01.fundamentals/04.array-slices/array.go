3514cb5a2136103d7a15db7aadf170c68712fbf3
Tan Chin Siang <chinsiang99@gmail.com>
2025-07-12 03:53:59 +0800

    docs

--- learning-go-with-test/01.fundamentals/04.array-slices/array.go
+++ learning-go-with-test/01.fundamentals/04.array-slices/array.go
@@ -8,6 +8,12 @@ func Sum(numbers []int) int {
 	return sum
 }
 
+// There's a new way to create a slice. make allows you to create a slice with a starting capacity of the len of the numbersToSum we need to work through. The length of a slice is the number of elements it holds len(mySlice), while the capacity is the number of elements it can hold in the underlying array cap(mySlice), e.g., make([]int, 0, 5) creates a slice with length 0 and capacity 5.
+// | Use case                                          | Use     |
+// | ------------------------------------------------- | ------- |
+// | Need actual current elements                      | `len()` |
+// | Need total allocated space (e.g., when appending) | `cap()` |
+
 func SumAll(numbersToSum ...[]int) []int {
 	lengthOfNumbers := len(numbersToSum)
 	sums := make([]int, lengthOfNumbers)
