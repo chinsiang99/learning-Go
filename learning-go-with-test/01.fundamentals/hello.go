06ff287404bbf1a44b1e3d8ec2519d0637952fcc
Tan Chin Siang <chinsiang99@gmail.com>
2025-07-12 02:03:39 +0800

    add new feat

--- learning-go-with-test/01.fundamentals/hello.go
+++ learning-go-with-test/01.fundamentals/hello.go
@@ -4,13 +4,19 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	} else {
+		return englishHelloPrefix + name
+	}
+
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
