eb9dbf1ee3d5b0b62e18d77a43a0e206054228dd
Tan Chin Siang <chinsiang99@gmail.com>
2025-07-12 02:15:47 +0800

    final refactor

--- learning-go-with-test/01.fundamentals/hello_test.go
+++ learning-go-with-test/01.fundamentals/hello_test.go
@@ -21,6 +21,12 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Luffy", "French")
+		want := "Bonjour, Luffy"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
